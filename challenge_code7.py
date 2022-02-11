63162830039a337695f6e68125041dd105a6d6ad
sarangsvkm <sarangsvkm@gmail.com>
2022-02-11 18:39:03 +0530

    python programs

--- challenge_code7.py
+++ challenge_code7.py
@@ -1,13 +1,12 @@
 def student_discount(a):
-    discount=a*(10/100)
-    discount_price=a-discount
+    discount_price=a-a*(10/100)
     return discount_price
 def regular_discount(b):
-    discount=student_discount(b)*5/100
-    discount_price=b-discount
+
+    discount_price=b-b*5/100
     return discount_price
 
 n=int(input("enter a price"))
 
-print("regular discount price is %d "%(regular_discount(n)),"student discount price is %d"%(student_discount(n)))
+print("regular discount price is %d "%(regular_discount(student_discount(n))),"student discount price is %d"%(student_discount(n)))
 
